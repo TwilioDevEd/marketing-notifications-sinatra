98b0a5f42a225152cfea3aed37469779663d5fc1
Ronualdo Maciel <user.email>
2016-02-17 14:38:14 -0300

    include subscribe and unsubscribe logic

--- app.rb
+++ app.rb
@@ -10,14 +10,8 @@ class Subscriber
 
   property :id, Serial
   property :phone_number, String
+  property :subscribed, Boolean
 
-  def initialize(phone_number)
-    @phone_number = phone_number
-  end
-
-  def subscribe
-    @subscribed = true
-  end
 end
 
 DataMapper.finalize
@@ -31,11 +25,29 @@ get '/hi' do
 end
 
 post '/subscriber' do
-  if params['Body'] == 'subscribe'
-    'subscribed'
-  elsif params['Body'] == 'unsubscribe'
-    'unsubscribed'
+  if params[:Body]
+    subscriber = create_or_update_subscriber(params)
+    if subscriber.subscribed
+      'You are now subscribed for updates.'
+    else
+      "You have unsubscribed from notifications. Test 'subscribe' to start receieving updates again"
+    end
+    subscriber.subscribed ? 'subscribed' : 'unsubscribed'
   else
     "Thanks for contacting TWBC! Text 'subscribe' if you would to receive updates via text message."
+  end
+end
+
+def is_subscription(command)
+  command == 'subscribe'
+end
+
+def create_or_update_subscriber(params)
+  subscriber = Subscriber.first(:phone_number => params[:From])
+  if subscriber
+    subscriber.update(:subscribed => is_subscription(params[:Body]))
+    subscriber
+  else
+    Subscriber.create(:phone_number => params[:From], :subscribed => is_subscription(params[:Body]))
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -10,13 +10,8 @@ describe 'marketing notifications' do
     Sinatra::Application
   end
 
-  it 'should be polite' do
-    get '/hi'
-    expect(last_response).to be_ok
-  end
-
   it 'should respond to the contact' do
-    allow(Subscriber).to receive(:all).and_return([])
+    allow(Subscriber).to receive(:first).and_return(nil)
 
     post 'subscriber', {:From => '999999999'}.to_json, 'CONTENT_TYPE' => 'application/json'
 
@@ -25,8 +20,9 @@ describe 'marketing notifications' do
   end
 
   it 'should register a new subscriber when asked' do
-    subscriber = Subscriber.new('99999999')
-    allow(Subscriber).to receive(:all).and_return([subscriber])
+    subscriber = double('subscriber', update: true, subscribed: true)
+    
+    allow(Subscriber).to receive(:first).and_return(subscriber)
 
     post 'subscriber', {:From => '999999999', :Body => 'subscribe'}.to_json, 'CONTENT_TYPE' => 'application/json'
 
@@ -35,10 +31,9 @@ describe 'marketing notifications' do
   end
 
   it 'should allow to unsubscriber' do
-    subscriber = Subscriber.new('99999999')
-    subscriber.subscribe
-
-    allow(Subscriber).to receive(:all).and_return([subscriber])
+    subscriber = double('subscriber', update: false, subscribed: false)
+    
+    allow(Subscriber).to receive(:first).and_return(subscriber)
 
     post 'subscriber', {:From => '999999999', :Body => 'unsubscribe'}.to_json, 'CONTENT_TYPE' => 'application/json'
 
