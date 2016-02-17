ebc2e984b03a743b02d22cadd4363794c7e12a7a
Ronualdo Maciel <user.email>
2016-02-17 10:21:01 -0300

    add subscriber endpoint

--- app.rb
+++ app.rb
@@ -1,5 +1,41 @@
 require 'sinatra'
+require 'data_mapper'
+require 'json'
+require 'rack/contrib'
+
+DataMapper.setup(:default, 'postgres://postgres:@localhost/marketing_notifications')
+
+class Subscriber
+  include DataMapper::Resource 
+
+  property :id, Serial
+  property :phone_number, String
+
+  def initialize(phone_number)
+    @phone_number = phone_number
+  end
+
+  def subscribe
+    @subscribed = true
+  end
+end
+
+DataMapper.finalize
+
+Subscriber.auto_upgrade!
+
+use ::Rack::PostBodyContentTypeParser
 
 get '/hi' do
   'hello world'
+end
+
+post '/subscriber' do
+  if params['Body'] == 'subscribe'
+    'subscribed'
+  elsif params['Body'] == 'unsubscribe'
+    'unsubscribed'
+  else
+    "Thanks for contacting TWBC! Text 'subscribe' if you would to receive updates via text message."
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../app'
 require 'rspec'
 require 'rack/test'
+require 'json'
 
 describe 'marketing notifications' do
   include Rack::Test::Methods
@@ -12,5 +13,36 @@ describe 'marketing notifications' do
   it 'should be polite' do
     get '/hi'
     expect(last_response).to be_ok
+  end
+
+  it 'should respond to the contact' do
+    allow(Subscriber).to receive(:all).and_return([])
+
+    post 'subscriber', {:From => '999999999'}.to_json, 'CONTENT_TYPE' => 'application/json'
+
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Thanks')
+  end
+
+  it 'should register a new subscriber when asked' do
+    subscriber = Subscriber.new('99999999')
+    allow(Subscriber).to receive(:all).and_return([subscriber])
+
+    post 'subscriber', {:From => '999999999', :Body => 'subscribe'}.to_json, 'CONTENT_TYPE' => 'application/json'
+
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('subscribed')
+  end
+
+  it 'should allow to unsubscriber' do
+    subscriber = Subscriber.new('99999999')
+    subscriber.subscribe
+
+    allow(Subscriber).to receive(:all).and_return([subscriber])
+
+    post 'subscriber', {:From => '999999999', :Body => 'unsubscribe'}.to_json, 'CONTENT_TYPE' => 'application/json'
+
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('unsubscribed')
   end
 end
