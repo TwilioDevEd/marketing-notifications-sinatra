494cdc7bde11f4e754351af8ebf0df811442c105
Ronualdo Maciel <user.email>
2016-02-22 13:17:35 -0300

    refactor model out of app file to its own file

--- app.rb
+++ app.rb
@@ -3,29 +3,9 @@ require 'data_mapper'
 require 'json'
 require 'rack/contrib'
 require 'twilio-ruby'
+require_relative 'model/subscriber'
 
 DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/marketing_notifications')
-
-class Subscriber
-  include DataMapper::Resource 
-
-  property :id, Serial
-  property :phone_number, String
-  property :subscribed, Boolean
-
-  def send_message(message, image_url)
-    @twilio_number = ENV['TWILIO_PHONE_NUMBER']
-    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-    message = @client.account.messages.create(
-      :from => @twilio_number,
-      :to => "+5581994596094",
-      :body => message,
-      :media_url => image_url
-    )       
-    puts message.to  
-  end
-
-end
 
 DataMapper.finalize
 
